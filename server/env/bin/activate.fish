979610ed0de706c1c41898481324742e806c4462
JoelCBarros <joel.cbarros14@gmail.com>
2024-07-18 19:09:05 -0400

    fixing bug on the server

--- server/env/bin/activate.fish
+++ server/env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/joel/fast-vue-crud/server/env"
+set -gx VIRTUAL_ENV "/home/joel/Downloads/fast-vue-crud/server/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
